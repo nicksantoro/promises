2e2e62fd332a27acb26e7ddf03e9a9dabb4a5f47
petermoser <peter.moser@gmail.com>
2018-09-29 10:59:40 -0700

    added api key and image url

--- singlePromise.js
+++ singlePromise.js
@@ -1,3 +1,8 @@
+const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
+const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const imgUrl = `https://image.tmdb.org/t/p/w185/`;
+
+
 let submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", function( event ){
     event.preventDefault();

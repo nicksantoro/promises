9390c1c93fafee05ea7cae768fcc1193f2ea3c50
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-09-29 10:51:02 -0700

    added submit bubtton handeler

---
+++ singlePromise.js
@@ -0,0 +1,5 @@
+let submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function( event ){
+    event.preventDefault();
+    console.log("it's working!")
+})
